e7667dd79e2ce511caa37dd01fc5012061f3ce93
Gabriella Martinez <martinez_gabriella04@student.smc.edu>
2026-07-01 12:45:17 -0700

    Added Subtract + determineOddOrEven functions

--- journal.js
+++ journal.js
@@ -1,12 +1,24 @@
 // Greet a user by name
 function greet(name) {
-  return "Hello, " + name + "!";
+    return "Hello, " + name + "!";
 }
 
 function sayHello() {
-  console.log("Hello, world!");
+    console.log("Hello, world!");
 }
 
 function add(a, b) {
-  return a + b;
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function determineOddOrEven(number) {
+    if (number % 2 == 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
 }
